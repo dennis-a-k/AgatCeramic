d78f92a4459db2f006e786549b71074d53753ab5
Dennis <kargopolov.d.a@gmail.com>
2024-03-03 14:49:46 +0300

    Баннеры

--- resources/views/components/banner/banner.blade.php
+++ resources/views/components/banner/banner.blade.php
@@ -1,48 +1,58 @@
-<div class="banner-area style-one pt-100px pb-100px">
+<div class="banner-area style-two pt-100px pb-100px">
     <div class="container">
         <div class="row">
-            <div class="col-md-6">
-                <x-banner.single-banner class="nth-child-1">
-                    <x-slot name="img" src="assets/images/banner/3.jpg" alt="banner"></x-slot>
+            <x-banner.single-banner class="nth-child-2 mb-30px mb-lm-30px mt-lm-30px">
+                <x-slot name="img" src="assets/images/banner/3.jpg" alt="banner"></x-slot>
 
-                    <x-slot name="title" class="nth-child-1">
-                        Керамогранит
-                    </x-slot>
+                <x-slot name="title" class="nth-child-2">
+                    Керамогранит
+                </x-slot>
 
-                    <x-slot name="category">
-                        От ведущих<br>
-                        производителей
-                    </x-slot>
+                <x-slot name="category">
+                    От ведущих<br>
+                    производителей
+                </x-slot>
 
-                    <x-slot name="btn" href="https://google.ru"></x-slot>
-                </x-banner.single-banner>
-            </div>
+                <x-slot name="url" href="https://ya.ru"></x-slot>
+            </x-banner.single-banner>
 
-            <div class="col-md-6">
-                <x-banner.single-banner class="nth-child-2 mb-30px mb-lm-30px mt-lm-30px">
-                    <x-slot name="img" src="assets/images/banner/4.jpg" alt="banner"></x-slot>
+            <x-banner.single-banner class="nth-child-2 mb-30px mb-lm-30px mt-lm-30px">
+                <x-slot name="img" src="assets/images/banner/4.jpg" alt="banner"></x-slot>
 
-                    <x-slot name="title" class="nth-child-2">
-                        Керамическая плитка
-                    </x-slot>
+                <x-slot name="title" class="nth-child-2">
+                    Керамическая плитка
+                </x-slot>
 
-                    <x-slot name="category"></x-slot>
+                <x-slot name="category"></x-slot>
 
-                    <x-slot name="btn" href="https://ya.ru"></x-slot>
-                </x-banner.single-banner>
+                <x-slot name="url" href="https://google.ru"></x-slot>
+            </x-banner.single-banner>
+        </div>
 
-                <x-banner.single-banner class="nth-child-2">
-                    <x-slot name="img" src="assets/images/banner/5.jpg" alt="banner"></x-slot>
+        <div class="row">
+            <x-banner.single-banner class="nth-child-2 mb-30px mb-lm-30px mt-lm-30px">
+                <x-slot name="img" src="assets/images/banner/5.jpg" alt="banner"></x-slot>
 
-                    <x-slot name="title" class="nth-child-3">
-                        Керамическая мозаика
-                    </x-slot>
+                <x-slot name="title" class="nth-child-2">
+                    Керамическая мозаика
+                </x-slot>
 
-                    <x-slot name="category"></x-slot>
+                <x-slot name="category"></x-slot>
 
-                    <x-slot name="btn" href="https://mail.ru"></x-slot>
-                </x-banner.single-banner>
-            </div>
+                <x-slot name="url" href="https://mail.ru"></x-slot>
+            </x-banner.single-banner>
+
+            <x-banner.single-banner class="nth-child-2 mb-30px mb-lm-30px mt-lm-30px">
+                <x-slot name="img" src="assets/images/banner/6.jpg" alt="banner"></x-slot>
+
+                <x-slot name="title" class="nth-child-2">
+                    Сантехника
+                </x-slot>
+
+                <x-slot name="category"></x-slot>
+
+                <x-slot name="url" href="https://google.ru"></x-slot>
+            </x-banner.single-banner>
         </div>
     </div>
 </div>
--- resources/views/components/banner/single-banner.blade.php
+++ resources/views/components/banner/single-banner.blade.php
@@ -1,8 +1,13 @@
-<div {{ $attributes->class('single-banner') }}>
-    <img {{ $img->attributes }}>
-    <div {{ $title->attributes->class('banner-content') }}>
-        <h3 class="title">{{ $title }}</h3>
-        <span class="category">{{ $category }}</span>
-        <a {{ $btn->attributes }} class="shop-link"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>
-    </div>
+<div class="col-md-6">
+    <a {{ $url->attributes }}>
+        <div class="single-banner nth-child-2 mb-30px mb-lm-30px">
+            <img {{ $img->attributes }}>
+            <a {{ $url->attributes }}>
+                <div class="banner-content nth-child-3">
+                    <h3 class="title">{{ $title }}</h3>
+                    <span class="category">{{ $category }}</span>
+                </div>
+            </a>
+        </div>
+    </a>
 </div>
