8de5e6af8111bb3671e3b912b38da6503a5c654e
Dennis <kargopolov.d.a@gmail.com>
2024-03-03 18:14:43 +0300

    слайдер брендов

---
+++ resources/views/components/sliders/brand-slider.blade.php
@@ -0,0 +1,20 @@
+<div class="brand-area pt-100px pb-100px">
+    <div class="container">
+        <div class="brand-slider swiper-container">
+            <div class="swiper-wrapper align-items-center">
+                <div class="swiper-slide brand-slider-item text-center">
+                    <a href="#"><img class=" img-fluid" src="assets/images/partner/01.png" alt="brand" /></a>
+                </div>
+                <div class="swiper-slide brand-slider-item text-center">
+                    <a href="#"><img class=" img-fluid" src="assets/images/partner/02.png" alt="brand" /></a>
+                </div>
+                <div class="swiper-slide brand-slider-item text-center">
+                    <a href="#"><img class=" img-fluid" src="assets/images/partner/03.png" alt="brand" /></a>
+                </div>
+                <div class="swiper-slide brand-slider-item text-center">
+                    <a href="#"><img class=" img-fluid" src="assets/images/partner/04.png" alt="brand" /></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
