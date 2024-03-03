9b5a45e58f35f2ddea627b97edd1bac86f8e7363
Dennis <kargopolov.d.a@gmail.com>
2024-03-03 19:52:08 +0300

    Работа с footer

---
+++ resources/views/components/footer/footer.blade.php
@@ -0,0 +1,94 @@
+<div class="footer-area">
+    <div class="footer-container">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-lg-6 mb-md-30px mb-lm-30px">
+                        <div class="single-wedge">
+                            <div class="footer-logo">
+                                <a href="{{ route('home') }}" class="footer-logo-text">Agat Ceramic</a>
+                            </div>
+                            <p class="about-text">БОЛЬШОЕ ОПИСАНИЕ МАГАЗИНА Lorem ipsum dolor sit amet consl adipisi
+                                elit, sed do
+                                eiusmod templ incididunt ut labore
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-lg-3 col-sm-6 mb-lm-30px pl-lg-40px">
+                        <div class="single-wedge">
+                            <h4 class="footer-herading">Меню сайта</h4>
+                            <div class="footer-links">
+                                <div class="footer-row">
+                                    <ul class="align-items-center">
+                                        <li class="li">
+                                            <a class="single-link" href="my-account.html">Керамогранит</a>
+                                        </li>
+                                        <li class="li">
+                                            <a class="single-link" href="my-account.html">Плитка</a>
+                                        </li>
+                                        <li class="li">
+                                            <a class="single-link" href="my-account.html">Мозаика</a>
+                                        </li>
+                                        <li class="li">
+                                            <a class="single-link" href="my-account.html">Дизайнерам</a>
+                                        </li>
+                                        <li class="li">
+                                            <a class="single-link" href="my-account.html">Доставка</a>
+                                        </li>
+                                        <li class="li">
+                                            <a class="single-link" href="my-account.html">Возврат</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-lg-3 col-sm-12">
+                        <div class="single-wedge">
+                            <h4 class="footer-herading">Контакты</h4>
+                            <div class="footer-links">
+                                <p class="phone">
+                                    <i class="fa fa-phone"></i>
+                                    <a href="tel:79999999999"> +7 (999) 999-99-99</a>
+                                </p>
+                                <p class="mail">
+                                    <i class="fa fa-envelope-o"></i>
+                                    <a href="mailto:zakaz@agatceramic.ru">zakaz@agatceramic.ru</a>
+                                </p>
+                            </div>
+                            <ul class="link-follow">
+                                <li>
+                                    <a class="m-0" title="Twitter" target="_blank" rel="noopener noreferrer"
+                                        href="#"><i class="fa fa-telegram" aria-hidden="true"></i></a>
+                                </li>
+                                <li>
+                                    <a title="Tumblr" target="_blank" rel="noopener noreferrer" href="#"><i
+                                            class="fa fa-whatsapp" aria-hidden="true"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="line-shape-top line-height-1">
+                    <div class="row flex-md-row-reverse align-items-center">
+                        <div class="col-md-6 text-center text-md-end">
+                            <div class="payment-mth">
+                                <a href="#"></a>
+                            </div>
+                        </div>
+                        <div class="col-md-6 text-center text-md-start">
+                            <p class="copy-text"> © <?= date('Y') ?> <strong>Agat Ceramic</strong></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
