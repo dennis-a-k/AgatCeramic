dedf629421c72b69e1bdd1500afbd270f416e630
Dennis <kargopolov.d.a@gmail.com>
2024-03-07 16:26:30 +0300

    Работа с информацией карточки товара

---
+++ resources/views/components/product/details-product.blade.php
@@ -0,0 +1,52 @@
+<div class="description-review-wrapper">
+    <div class="description-review-topbar nav">
+        <button data-bs-toggle="tab" data-bs-target="#des-details2">Характеристики</button>
+        <button class="active" data-bs-toggle="tab" data-bs-target="#des-details1">Описание</button>
+    </div>
+    <div class="tab-content description-review-bottom">
+        <div id="des-details2" class="tab-pane">
+            <div class="product-anotherinfo-wrapper text-start d-flex">
+                <ul>
+                    <li><span>Категория:</span></li>
+                    <li><span>Размер:</span></li>
+                    <li><span>Цвет:</span></li>
+                    <li><span>Поверхность:</span></li>
+                    <li><span>Узор:</span></li>
+                    <li><span>Производитель:</span></li>
+                    <li><span>Артикул производителя:</span></li>
+                    <li><span>Страна:</span></li>
+                    <li><span>Кратность отгрузки:</span></li>
+                    <li><span>Количество в упаковке:</span></li>
+                    <li><span>Вес упаковки:</span></li>
+                    <li class="product-anotherinfo-img"><a href=""><img src="assets/images/partner/03.png"
+                                alt="partner"></a></li>
+                </ul>
+
+                <ul>
+                    <li><a href="">Керамогранит</a></li>
+                    <li>120х70 мм</li>
+                    <li>Розовый</li>
+                    <li>Матовая</li>
+                    <li>Под мрамор</a></li>
+                    <li><a href="">Антил</a></li>
+                    <li>88888888</li>
+                    <li>Россия</li>
+                    <li>1 упаковка (1,6 м2)</li>
+                    <li>10 шт.</li>
+                    <li>28,14</li>
+                </ul>
+            </div>
+        </div>
+        <div id="des-details1" class="tab-pane active">
+            <div class="product-description-wrapper">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius tempor incidid
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip efgx ea co consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occae cupidatat non proident, sunt in culpa qui
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/components/product/information-product.blade.php
@@ -0,0 +1,35 @@
+<div class="pro-details-categories-info pro-details-same-style d-flex m-0">
+    <span>Артикул:</span>
+    <ul class="d-flex">
+        <li>
+            Ch-256xl
+        </li>
+    </ul>
+</div>
+
+<div class="pro-details-categories-info pro-details-same-style d-flex m-0">
+    <span>Цвет: </span>
+    <ul class="d-flex">
+        <li>
+            Розовый
+        </li>
+    </ul>
+</div>
+
+<div class="pro-details-categories-info pro-details-same-style d-flex m-0">
+    <span>Размер: </span>
+    <ul class="d-flex">
+        <li>
+            120х70 мм
+        </li>
+    </ul>
+</div>
+
+<div class="pro-details-categories-info pro-details-same-style d-flex m-0">
+    <span>Поверхность: </span>
+    <ul class="d-flex">
+        <li>
+            Матовая
+        </li>
+    </ul>
+</div>
