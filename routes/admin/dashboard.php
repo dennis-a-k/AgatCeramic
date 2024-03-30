041ba15aae2aa9beb59779edde551e8b8c88902d
Dennis <kargopolov.d.a@gmail.com>
2024-03-30 18:34:44 +0300

    правки

--- routes/admin/dashboard.php
+++ routes/admin/dashboard.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/admin/dashboard', function () {
 //     return view('pages.admin.dashboard');
 // })->middleware(['auth', 'verified'])->name('admin.dashboard');
-Route::get('/admin/dashboard', function () {
+Route::get('/dashboard', function () {
     return view('pages.admin.dashboard');
 })->name('admin.dashboard');
