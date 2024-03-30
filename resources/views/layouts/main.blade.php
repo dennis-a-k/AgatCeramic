9103321021ec087d998901803bfc51c3f4af1b4d
Dennis <kargopolov.d.a@gmail.com>
2024-03-30 19:38:07 +0300

    правки

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -187,18 +187,18 @@
     </div>
     <!-- Modal end -->
 
-    <script src="assets/js/vendor/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
-    <script src="assets/js/vendor/jquery-migrate-3.3.2.min.js"></script>
-    <script src="assets/js/vendor/modernizr-3.11.2.min.js"></script>
-    <script src="assets/js/plugins/jquery.countdown.min.js"></script>
-    <script src="assets/js/plugins/swiper-bundle.min.js"></script>
-    <script src="assets/js/plugins/scrollUp.js"></script>
-    <script src="assets/js/plugins/venobox.min.js"></script>
-    <script src="assets/js/plugins/jquery-ui.min.js"></script>
-    <script src="assets/js/plugins/mailchimp-ajax.js"></script>
+    <script src="{{ URL::asset('assets/js/vendor/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/vendor/jquery-3.6.0.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/vendor/jquery-migrate-3.3.2.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/vendor/modernizr-3.11.2.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/plugins/jquery.countdown.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/plugins/swiper-bundle.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/plugins/scrollUp.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/plugins/venobox.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/plugins/jquery-ui.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/plugins/mailchimp-ajax.js') }}"></script>
 
-    <script src="assets/js/main.js"></script>
+    <script src="{{ URL::asset('assets/js/main.js') }}"></script>
 
     @yield('js')
 </body>
