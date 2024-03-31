6f4cfd71552c9e33632ad31c9ab432a6140c9c26
Dennis <kargopolov.d.a@gmail.com>
2024-03-31 18:12:24 +0300

    Revert "правки"
    This reverts commit 9b58d9c5e89900108f0166a5729a85f91ec9e1a9.

--- resources/views/pages/admin/goods/goods.blade.php
+++ resources/views/pages/admin/goods/goods.blade.php
@@ -10,6 +10,8 @@
         }
     </style>
 
+    <link rel="stylesheet" href="{{ URL::asset('assets/adminlte/plugins/jsgrid/jsgrid.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/adminlte/plugins/jsgrid/jsgrid-theme.min.css') }}">
 @endsection
 
 @section('content-header')
@@ -82,7 +84,7 @@
                             </tbody>
                         </table>
 
-
+                        <div id="jsGrid1"></div>
                     </div>
                 </div>
             @endif
@@ -110,6 +112,45 @@
             modal.find('.modal-title').text('Удаление товара арт: ' + product['article']);
             modal.find('.modal-text').text('Удалить ' + product['title'] + '?');
             modal.find('.modal-id').attr('value', product['id']);
+        });
+
+        $("#jsGrid1").jsGrid({
+            height: "100%",
+            width: "100%",
+
+            sorting: true,
+            paging: true,
+
+            data: db.clients,
+
+            fields: [{
+                    name: "Name",
+                    type: "text",
+                    width: 150
+                },
+                {
+                    name: "Age",
+                    type: "number",
+                    width: 50
+                },
+                {
+                    name: "Address",
+                    type: "text",
+                    width: 200
+                },
+                {
+                    name: "Country",
+                    type: "select",
+                    items: db.countries,
+                    valueField: "Id",
+                    textField: "Name"
+                },
+                {
+                    name: "Married",
+                    type: "checkbox",
+                    title: "Is Married"
+                }
+            ]
         });
     </script>
 @endsection
