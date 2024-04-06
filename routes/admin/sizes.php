5e8174d26a3be66e92b8f245efd56e96dabe013a
Dennis <kargopolov.d.a@gmail.com>
2024-04-06 21:22:11 +0300

    правки

--- routes/admin/sizes.php
+++ routes/admin/sizes.php
@@ -7,7 +7,7 @@ Route::group([
     'prefix' => '/admin/sizes',
     // 'middleware' => 'auth',
 ], function () {
-    Route::get('/list', [SizesController::class, 'index'])->name('sizes.list');
+    Route::get('/', [SizesController::class, 'index'])->name('sizes.list');
     Route::post('/create', [SizesController::class, 'store'])->name('sizes.store');
     Route::patch('/', [SizesController::class, 'update'])->name('sizes.update');
     Route::delete('/', [SizesController::class, 'destroy'])->name('sizes.destroy');
