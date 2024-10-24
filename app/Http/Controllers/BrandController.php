48144263445200637d8a7f6a8fd00ecef9dbd5cd
Dennis <kargopolov.d.a@gmail.com>
2024-10-24 16:47:24 +0300

    рефакторинг кода при помощи  ИИ

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -10,68 +10,59 @@ use Illuminate\Support\Str;
 
 class BrandController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $brands = Brand::query()->orderBy('title', 'ASC')->get();
-        return view('pages.admin.brands', ['brands' => $brands]);
+        $brands = Brand::orderBy('title', 'ASC')->get();
+        return view('pages.admin.brands', compact('brands'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(BrandRequest $request)
     {
-        $data = $request->validated();
-
-        $brand = new Brand();
-        $brand->title = $data['title'];
-
-        if ($request->hasFile('img')) {
-            $slug = Str::slug($brand->title );
-            $name = $slug . '_brand' . '.' . $data['img']->getClientOriginalExtension();
-            $brand->img = Storage::disk('public')->putFileAs('/brands', $data['img'], $name);
-        }
-
+        $brand = new Brand($request->validated());
+        $this->handleImageUpload($brand, $request);
         $brand->save();
 
         return back()->with('status', 'brand-created');
     }
 
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(BrandRequest $request)
     {
-        $data = $request->validated();
-
         $brand = Brand::findOrFail($request->id);
-        $brand->title = $data['title'];
-
-        if ($request->hasFile('img')) {
-            if ($brand->img) {
-                Storage::delete($brand->img);
-            }
-
-            $slug = Str::slug($brand->title );
-            $name = $slug . '_brand' . '.' . $data['img']->getClientOriginalExtension();
-            $brand->img = Storage::disk('public')->putFileAs('/brands', $data['img'], $name);
-        }
-
+        $brand->fill($request->validated());
+        $this->handleImageUpload($brand, $request, true);
         $brand->save();
 
         return back()->with('status', 'brand-updated');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Request $request)
     {
-        Brand::find($request->id)->delete();
-        return back();
+        $brand = Brand::findOrFail($request->id);
+        $this->deleteImage($brand);
+        $brand->delete();
+
+        return back()->with('status', 'brand-deleted');
+    }
+
+    private function handleImageUpload(Brand $brand, BrandRequest $request, bool $isUpdate = false)
+    {
+        if ($request->hasFile('img')) {
+            if ($isUpdate && $brand->img) {
+                $this->deleteImage($brand);
+            }
+
+            $slug = Str::slug($brand->title);
+            $extension = $request->file('img')->getClientOriginalExtension();
+            $fileName = "{$slug}_brand.{$extension}";
+            $request->file('img')->storeAs('public/brands', $fileName);
+            $brand->img = $fileName;
+        }
+    }
+
+    private function deleteImage(Brand $brand)
+    {
+        if ($brand->img) {
+            Storage::disk('public')->delete('brands/' . $brand->img);
+        }
     }
 }
