25521a4f4680a10829809977677681ffaf06b2cf
Dennis <kargopolov.d.a@gmail.com>
2024-10-29 16:34:43 +0300

    Распределение бренжов по алфавиту в меню навигации

--- app/View/Composers/ViewComposers/BrandComposer.php
+++ app/View/Composers/ViewComposers/BrandComposer.php
@@ -9,9 +9,26 @@ class BrandComposer
 {
     public function compose(View $view)
     {
-        $brands = Brand::all();
+        $brands = Brand::orderBy('title')->get();
+        $groupedBrands = $brands->groupBy(function($brand) {
+            $firstChar = mb_strtoupper(mb_substr($brand->title, 0, 1));
+            // Проверяем, является ли первый символ цифрой
+            if (is_numeric($firstChar)) {
+                return '0-9';
+            }
+            // Проверяем, является ли символ латинским
+            if (preg_match('/^[A-Z]$/', $firstChar)) {
+                return $firstChar;
+            }
+            // Все остальные символы (кириллица и прочие) группируем вместе
+            return 'РУС';
+        })->sortKeys();
+        // Сортируем бренды внутри групп
+        $groupedBrands = $groupedBrands->map(function($brands) {
+            return $brands->sortBy('title');
+        });
         $view->with([
-            'brands' => $brands,
+            'groupedBrands' => $groupedBrands,
         ]);
     }
 }
