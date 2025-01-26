66e6f6abbbc3681e1e0d9b14ab0df3a1ae8f5fed
Dennis <kargopolov.d.a@gmail.com>
2025-01-26 18:50:25 +0300

    Верификация пользователя

--- app/Http/Controllers/Auth/Admin/UserController.php
+++ app/Http/Controllers/Auth/Admin/UserController.php
@@ -5,9 +5,19 @@ namespace App\Http\Controllers\Auth\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
+    public function index()
+    {
+        $users = User::orderBy('name', 'ASC')->get();
+        return view('pages.admin.users', compact('users'));
+    }
+
     public function create()
     {
         return view('pages.admin.auth.register');
@@ -17,11 +27,36 @@ class UserController extends Controller
     {
         $data = $request->validated();
         $user = User::create($data);
+        event(new Registered($user));
+        Auth::login($user);
         return back()->with('status', 'user-created');
     }
 
     public function login()
     {
         return view('pages.admin.auth.login');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('login');
+    }
+
+    public function notice()
+    {
+        return view('pages.admin.auth.verify-email');
+    }
+
+    public function verify(EmailVerificationRequest $request)
+    {
+        $request->fulfill();
+        return redirect()->route('orders.list');
+    }
+
+    public function send(Request $request)
+    {
+        $request->user()->sendEmailVerificationNotification();
+        return back()->with('status', 'verification-send');
     }
 }
--- routes/admin/auth/auth.php
+++ routes/admin/auth/auth.php
@@ -3,11 +3,22 @@
 use App\Http\Controllers\Auth\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/admin-panel/login', [UserController::class, 'login'])->middleware('guest')->name('login');
+
 Route::group([
     'prefix' => '/admin-panel',
     // 'middleware' => 'auth',
 ], function () {
-    Route::get('/login', [UserController::class, 'login'])->name('login');
+    Route::get('/email/verify', [UserController::class, 'notice'])->name('verification.notice');
+    Route::get('/email/verify/{id}/{hash}', [UserController::class, 'verify'])->name('verification.verify');
+    Route::post('/email/verification-notification', [UserController::class, 'send'])->middleware('throttle:6,1')->name('verification.send');
+});
+
+Route::group([
+    'prefix' => '/admin-panel',
+    // 'middleware' => ['auth', 'verified']
+], function () {
     Route::get('/register', [UserController::class, 'create'])->name('register');
     Route::post('/register', [UserController::class, 'store'])->name('user.store');
+    Route::get('/logout', [UserController::class, 'logout'])->name('logout');
 });
