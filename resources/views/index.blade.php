eb3abfd585532b72484d6dec7b79b9c6e34e5e4e
Dennis <kargopolov.d.a@gmail.com>
2025-04-08 21:40:48 +0300

    Замена изображения лого в мета-теге

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
     <meta property="og:description"
         content="Agat Ceramic — интернет-магазин керамической плитки, керамогранита и мозаики в Москве и Московской области. В интернет-магазине Agat Ceramic вы найдете премиальную керамическую плитку, керамогранит и мозаику от ведущих мировых производителей. Мы предлагаем эксклюзивные коллекции для создания уникального дизайна интерьера: напольную и настенную плитку, износостойкий керамогранит для коммерческих и жилых помещений, а также стильную мозаику для акцентных решений.">
     <meta property="og:type" content="website">
-    <meta property="og:image" content="{{ asset('assets/images/stock/logo.svg') }}">
+    <meta property="og:image" content="{{ asset('assets/images/stock/logo.png') }}">
     <meta property="og:site_name" content="{{ config('app.name') }}">
     <meta property="og:locale" content="ru_RU">
     <meta property="og:url" content="{{ route('home') }}">
