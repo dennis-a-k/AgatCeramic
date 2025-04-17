252bf916687487dcc05b0b9ec279f6ccf215f464
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-04-17 12:15:04 +0300

    Добавление модального окна на сайте для заявок на звонок

--- resources/views/components/header/header-top.blade.php
+++ resources/views/components/header/header-top.blade.php
@@ -12,7 +12,11 @@
                     <ul>
                         <li><a href="{{ route('delivery') }}">Оплата и доставка</a></li>
                         <li><a href="{{ route('return') }}">Возврат и замена</a></li>
-                        <li><a href="{{ route('return') }}">Заказать звонок</a></li>
+                        <li>
+                            <a href="#" class="modal-call" data-link-action="modal-call" data-bs-toggle="modal" data-bs-target="#modalCall">
+                                Заказать звонок
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -21,7 +25,8 @@
                 <div class="top-nav">
                     <ul>
                         <li><a href="tel:+79999999999" class="phone-link"><i class="fa fa-phone"></i> +7 (999) 999-99-99</a></li>
-                        <li><a href="mailto:zakaz@agatceramic.ru">
+                        <li>
+                            <a href="mailto:zakaz@agatceramic.ru">
                                 <i class="fa fa-envelope-o"></i>
                                 zakaz@agatceramic.ru
                             </a>
