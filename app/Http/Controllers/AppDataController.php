e5f98d2b01a6c4a0508473db489346faa36df524
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-04-21 11:42:20 +0300

    Изменение данных компании

--- app/Http/Controllers/AppDataController.php
+++ app/Http/Controllers/AppDataController.php
@@ -9,8 +9,115 @@ class AppDataController extends Controller
 {
     public function index()
     {
-        $data = AppData::all();
-        // dd($data);
+        $data = AppData::first();
         return view('pages.admin.app-data', compact('data'));
+    }
+
+    public function updatePhone(Request $request)
+    {
+        AppData::updateOrCreate(
+            ['id' => 1],
+            ['app_phone' => $request->app_phone],
+        );
+        return redirect()->back()->with('status', 'phone-updated');
+    }
+
+    public function updateEmail(Request $request)
+    {
+        AppData::updateOrCreate(
+            ['id' => 1],
+            ['app_email' => $request->app_email],
+        );
+        return redirect()->back()->with('status', 'email-updated');
+    }
+
+    public function updateOrganization(Request $request)
+    {
+        AppData::updateOrCreate(
+            ['id' => 1],
+            ['organization' => $request->organization],
+        );
+        return redirect()->back()->with('status', 'organization-updated');
+    }
+
+    public function updateINN(Request $request)
+    {
+        AppData::updateOrCreate(
+            ['id' => 1],
+            ['inn' => $request->inn],
+        );
+        return redirect()->back()->with('status', 'inn-updated');
+    }
+
+    public function updateOGRN(Request $request)
+    {
+        AppData::updateOrCreate(
+            ['id' => 1],
+            ['ogrn' => $request->ogrn],
+        );
+        return redirect()->back()->with('status', 'ogrn-updated');
+    }
+
+    public function updateOKATO(Request $request)
+    {
+        AppData::updateOrCreate(
+            ['id' => 1],
+            ['okato' => $request->okato],
+        );
+        return redirect()->back()->with('status', 'okato-updated');
+    }
+
+    public function updateOKPO(Request $request)
+    {
+        AppData::updateOrCreate(
+            ['id' => 1],
+            ['okpo' => $request->okpo],
+        );
+        return redirect()->back()->with('status', 'okpo-updated');
+    }
+
+    public function updateBank(Request $request)
+    {
+        AppData::updateOrCreate(
+            ['id' => 1],
+            ['bank' => $request->bank],
+        );
+        return redirect()->back()->with('status', 'bank-updated');
+    }
+
+    public function updateBIK(Request $request)
+    {
+        AppData::updateOrCreate(
+            ['id' => 1],
+            ['bik' => $request->bik],
+        );
+        return redirect()->back()->with('status', 'bik-updated');
+    }
+
+    public function updateK_s(Request $request)
+    {
+        AppData::updateOrCreate(
+            ['id' => 1],
+            ['k_s' => $request->k_s],
+        );
+        return redirect()->back()->with('status', 'k_s-updated');
+    }
+
+    public function updateR_s(Request $request)
+    {
+        AppData::updateOrCreate(
+            ['id' => 1],
+            ['r_s' => $request->r_s],
+        );
+        return redirect()->back()->with('status', 'r_s-updated');
+    }
+
+    public function updateAdress(Request $request)
+    {
+        AppData::updateOrCreate(
+            ['id' => 1],
+            ['adress' => $request->adress],
+        );
+        return redirect()->back()->with('status', 'adress-updated');
     }
 }
--- routes/admin/app-data.php
+++ routes/admin/app-data.php
@@ -8,7 +8,16 @@ Route::group([
     'middleware' => ['auth', 'verified'],
 ], function () {
     Route::get('/', [AppDataController::class, 'index'])->name('app.data');
-    // Route::post('/create', [BrandController::class, 'store'])->name('brands.store');
-    // Route::patch('/', [BrandController::class, 'update'])->name('brands.update');
-    // Route::delete('/', [BrandController::class, 'destroy'])->name('brands.destroy');
+    Route::post('/update-phone', [AppDataController::class, 'updatePhone'])->name('data.update.phone');
+    Route::post('/update-email', [AppDataController::class, 'updateEmail'])->name('data.update.email');
+    Route::post('/update-organization', [AppDataController::class, 'updateOrganization'])->name('data.update.organization');
+    Route::post('/update-inn', [AppDataController::class, 'updateINN'])->name('data.update.inn');
+    Route::post('/update-ogrn', [AppDataController::class, 'updateOGRN'])->name('data.update.ogrn');
+    Route::post('/update-okato', [AppDataController::class, 'updateOKATO'])->name('data.update.okato');
+    Route::post('/update-okpo', [AppDataController::class, 'updateOKPO'])->name('data.update.okpo');
+    Route::post('/update-bank', [AppDataController::class, 'updateBank'])->name('data.update.bank');
+    Route::post('/update-bik', [AppDataController::class, 'updateBIK'])->name('data.update.bik');
+    Route::post('/update-k_s', [AppDataController::class, 'updateK_s'])->name('data.update.k_s');
+    Route::post('/update-r_s', [AppDataController::class, 'updateR_s'])->name('data.update.r_s');
+    Route::post('/update-adress', [AppDataController::class, 'updateAdress'])->name('data.update.adress');
 });
