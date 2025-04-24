f0e49d5c89e9edfe31e6e37f7320986dd8466b3f
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-04-24 10:41:30 +0300

    Правки в банере распродажи

--- resources/views/components/banner/sail-banner.blade.php
+++ resources/views/components/banner/sail-banner.blade.php
@@ -1,9 +1,8 @@
-<section class="container h-100 fashion-area py-5" data-bg-banner="assets/images/fashion/02.jpg">
-    <h2 class="text-center d-none my-5">Распродажа</h2>
-    <div class="my-5">
+<section class="fashion-area" data-bg-banner="assets/images/banner/sail.png" style="background-repeat: no-repeat; background-size: cover;">
+    <div class="container h-100">
         <div class="row justify-content-center align-items-center h-100">
             <div class="col-12 text-center">
-                <h2 class="title"> <span>Распродажа</span>Успейти купить</h2>
+                <h2 class="title"><span>Распродажа</span>Успейти купить</h2>
                 <a href="{{ route('sale') }}" class="btn btn-primary text-capitalize m-auto">
                     Распродажа
                 </a>
