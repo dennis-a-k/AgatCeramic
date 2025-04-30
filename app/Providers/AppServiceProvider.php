e2219dea7886de600fb67354dbbde2288ec288c1
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-04-30 15:11:19 +0300

    Рефакторинг AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,30 +11,19 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        $this->app->bind(FilterService::class, fn () => new FilterService());
-        $this->app->bind(SessionService::class, fn () => new SessionService());
+        $this->app->singleton(FilterService::class, fn () => new FilterService());
+        $this->app->singleton(SessionService::class, fn () => new SessionService());
+        $this->app->singleton('appData', fn () => Cache::remember('appData', 3600,
+            fn () => AppData::first()
+        ));
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        $this->app->singleton('appData', function () {
-            return AppData::first();
-        });
-
         View::composer('*', function ($view) {
             $view->with('appData', app('appData'));
-        });
-
-        $appData = Cache::remember('appData', 3600, function () {
-            return AppData::first();
         });
     }
 }
