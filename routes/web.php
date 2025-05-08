d8fa284e9fd457e2a5db5948d83d9de216f09dd2
Dennis <kargopolov.d.a@gmail.com>
2025-05-08 11:12:31 +0300

    Убрал воможность работать с категориями так как они теперь по дефолту будут

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,34 @@
 <?php
 
-require __DIR__.'/pages.php';
-require __DIR__.'/cart.php';
+require __DIR__ . '/pages.php';
+require __DIR__ . '/cart.php';
 
 
-require __DIR__.'/santekhnika.php';
+require __DIR__ . '/santekhnika.php';
 
-require __DIR__.'/goods.php';
-require __DIR__.'/category.php';
-require __DIR__.'/brand.php';
+require __DIR__ . '/goods.php';
+require __DIR__ . '/category.php';
+require __DIR__ . '/brand.php';
 
-require __DIR__.'/search.php';
-require __DIR__.'/filters.php';
-require __DIR__.'/sale.php';
-require __DIR__.'/call.php';
+require __DIR__ . '/search.php';
+require __DIR__ . '/filters.php';
+require __DIR__ . '/sale.php';
+require __DIR__ . '/call.php';
 
-require __DIR__.'/admin/goods.php';
-require __DIR__.'/admin/kleya.php';
-require __DIR__.'/admin/zatirka.php';
-require __DIR__.'/admin/category.php';
-require __DIR__.'/admin/sizes.php';
-require __DIR__.'/admin/pattern.php';
-require __DIR__.'/admin/texture.php';
-require __DIR__.'/admin/collection.php';
-require __DIR__.'/admin/country.php';
-require __DIR__.'/admin/color.php';
-require __DIR__.'/admin/brand.php';
-require __DIR__.'/admin/orders.php';
-require __DIR__.'/admin/editor.php';
-require __DIR__.'/admin/user.php';
-require __DIR__.'/admin/calls.php';
-require __DIR__.'/admin/app-data.php';
+require __DIR__ . '/admin/goods.php';
+require __DIR__ . '/admin/kleya.php';
+require __DIR__ . '/admin/zatirka.php';
+require __DIR__ . '/admin/sizes.php';
+require __DIR__ . '/admin/pattern.php';
+require __DIR__ . '/admin/texture.php';
+require __DIR__ . '/admin/collection.php';
+require __DIR__ . '/admin/country.php';
+require __DIR__ . '/admin/color.php';
+require __DIR__ . '/admin/brand.php';
+require __DIR__ . '/admin/orders.php';
+require __DIR__ . '/admin/editor.php';
+require __DIR__ . '/admin/user.php';
+require __DIR__ . '/admin/calls.php';
+require __DIR__ . '/admin/app-data.php';
 
-require __DIR__.'/admin/auth/auth.php';
+require __DIR__ . '/admin/auth/auth.php';
