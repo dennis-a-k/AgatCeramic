cfa75d9167761b0a0d32635f39c80b30f9c5e1d6
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-05-21 13:15:53 +0300

    Сделал вывод в алфавитном меню брендов только керамики

--- app/View/Composers/ViewComposers/BrandComposer.php
+++ app/View/Composers/ViewComposers/BrandComposer.php
@@ -9,8 +9,21 @@ class BrandComposer
 {
     public function compose(View $view)
     {
-        $brands = Brand::orderBy('title')->get();
-        $groupedBrands = $brands->groupBy(function($brand) {
+        $includedCategories = [
+            config('categories.keramogranit'),
+            config('categories.plitka'),
+            config('categories.mozaika'),
+            config('categories.klinker'),
+            config('categories.stupeni'),
+        ];
+
+        $brands = Brand::whereHas('goods', function ($query) use ($includedCategories) {
+            $query->whereHas('category', function ($q) use ($includedCategories) {
+                $q->whereIn('title', $includedCategories);
+            });
+        })->orderBy('title')->get();
+
+        $groupedBrands = $brands->groupBy(function ($brand) {
             $firstChar = mb_strtoupper(mb_substr($brand->title, 0, 1));
             // Проверяем, является ли первый символ цифрой
             if (is_numeric($firstChar)) {
@@ -24,14 +37,14 @@ class BrandComposer
             return 'РУС';
         })->sortKeys();
         // Сортируем бренды внутри групп
-        $groupedBrands = $groupedBrands->map(function($brands) {
+        $groupedBrands = $groupedBrands->map(function ($brands) {
             return $brands->sortBy(fn($brand) => mb_strtolower($brand->title))
-                          ->map(function($brand) {
-                              // Делаем первую букву заглавной
-                              $brand->title = mb_strtoupper(mb_substr($brand->title, 0, 1)) . mb_substr($brand->title, 1);
-                              return $brand;
-                          })
-                          ->values();
+                ->map(function ($brand) {
+                    // Делаем первую букву заглавной
+                    $brand->title = mb_strtoupper(mb_substr($brand->title, 0, 1)) . mb_substr($brand->title, 1);
+                    return $brand;
+                })
+                ->values();
         });
         $view->with([
             'groupedBrands' => $groupedBrands,
