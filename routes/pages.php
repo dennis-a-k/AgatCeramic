dd0909067d647e72e9429a8826448543bbd5d852
Dennis-Job <kargopolov.ogo1@gmail.com>
2025-08-07 16:35:32 +0300

    Второй вариант дизайна страницы для дизайнеров

--- routes/pages.php
+++ routes/pages.php
@@ -18,6 +18,9 @@ Route::get('/return', function () {
 Route::get('/partnerships', function () {
     return view('pages.partnerships');
 })->name('partnerships');
+Route::get('/partnerships2', function () {
+    return view('pages.partnerships2');
+})->name('partnerships2');
 
 Route::get('/contact', function () {
     return view('pages.contact');
