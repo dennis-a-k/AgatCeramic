833b987dff66526df3ebc0536b6586313c7bd89f
dennis-a-k <kargopolov.d.a@gmail.com>
2025-08-09 22:13:33 +0300

    Пакеты обновлений программы

--- routes/admin/auth/auth.php
+++ routes/admin/auth/auth.php
@@ -16,8 +16,8 @@ Route::group([
     Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
     Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->middleware('throttle:6,1')->name('password.email');
 
-    Route::get('/reset-password/{token}',[NewPasswordController::class, 'create'])->name('password.reset');
-    Route::post('/reset-password',[NewPasswordController::class, 'store'])->name('password.update');
+    Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
+    Route::post('/reset-password', [NewPasswordController::class, 'store'])->name('password.update');
 });
 
 Route::group([
